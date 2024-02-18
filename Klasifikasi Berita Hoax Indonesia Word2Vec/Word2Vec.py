b873c72165117f827b829468220748ca10aee06b
FaiqFdh <94748589+FaiqFdh@users.noreply.github.com>
2024-02-18 20:03:52 +0700

    Delete Klasifikasi Berita Hoax Indonesia Word2Vec directory

--- Klasifikasi Berita Hoax Indonesia Word2Vec/Word2Vec.py
+++
@@ -1,5 +0,0 @@
-import zipfile
-
-zip_ref = zipfile.ZipFile('C:\\Users\\user\\PycharmProjects\\Deep_Learning\\Klasifikasi Berita Hoax Indonesia Word2Vec\\Indonesia word2vec embbeding.zip', 'r')
-zip_ref.extractall('C:\\Users\\user\\PycharmProjects\\Deep_Learning\\Klasifikasi Berita Hoax Indonesia Word2Vec\\Indo Word2Vec')
-zip_ref.close()
